b9c7c09603feff7cb39a4b80a6f24a44c95e78d6
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-10-25 15:55:05 +0300

    binding & immutability

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+// mod helper;
+
+// use helper::starter::multiply;
+
+// fn main() {
+//     let result = multiply(5, 6);
+//     println!("The result is: {}", result);
+// }
+
+//code
+fn main (){
+    let x: i32 = 9;
+    let y: i32 = 5;
+    assert_eq!(x,y);
+    println!("success")
+}
