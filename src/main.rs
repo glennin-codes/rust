9f8d23a13cb7ee648f88cd2eefa85feed2311d38
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-11-28 00:48:03 +0300

    infinite loop

--- src/main.rs
+++ src/main.rs
@@ -964,3 +964,25 @@ enum Message{
 
 //     }
 // }
+//infinite looping
+fn main(){
+    let mut count:i32=0;
+    'outer:loop{
+        'inner1:loop{
+                    if count >= 20{
+                        break 'inner1;
+                    }
+                        count+=2;
+        }
+              count+=2;
+        '_inner2:loop{
+                if count >=30{
+                    break 'outer;
+                }
+                continue 'outer;
+        }
+
+    }
+    assert_eq!(count,30);
+    println!("succes! {}",count);
+}
